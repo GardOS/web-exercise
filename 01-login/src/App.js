35fe0a63875d11261b7af66aa82d8f6b7eefd5fc
GardOS <Gardo1506@gmail.com>
2018-10-05 16:27:02 +0000

    Fix CSS

--- 01-login/src/App.js
+++ 01-login/src/App.js
@@ -1,25 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container">
+        <h1>Login!</h1>
+        <form onSubmit={e => {
+            e.preventDefault();
+
+            console.log('state', JSON.stringify(this.state));
+            console.log('submitted');
+          }}>
+          <label>
+            Name:
+            <input type="text" name="name" />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
       </div>
     );
   }
