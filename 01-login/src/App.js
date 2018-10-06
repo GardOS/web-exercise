876ea12fada7e4fa46b99cc0aedc577b5fc8086b
GardOS <Gardo1506@gmail.com>
2018-10-06 13:38:36 +0000

    Add html for form

--- 01-login/src/App.js
+++ 01-login/src/App.js
@@ -2,25 +2,67 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
+
+    this.state = {
+      username: '',
+      password: '',
+    };
   }
 
   render() {
     return (
       <div className="container">
-        <h1>Login!</h1>
         <form onSubmit={e => {
-            e.preventDefault();
+          e.preventDefault();
 
-            console.log('state', JSON.stringify(this.state));
-            console.log('submitted');
-          }}>
+          console.log('state', JSON.stringify(this.state));
+          console.log('submitted');
+        }}>
+          <h1>Sign in!</h1>
+          <div className="form-group">
+            <label htmlFor="email-input">
+              Email address
+            </label>
+            <input
+              id="email-input"
+              type="email"
+              className="form-control"
+              placeholder="Enter email"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password-input">
+              Password
+            </label>
+            <input
+              id="password-input"
+              type="password"
+              className="form-control"
+              placeholder="Enter password"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="retype-password-input">
+              Retype password
+            </label>
+            <input
+              id="retype-password-input"
+              type="password"
+              className="form-control"
+              placeholder="Retype password"
+            />
+          </div>
           <label>
-            Name:
-            <input type="text" name="name" />
+            <input 
+              id="terms-and-conditions-check"              
+              type="checkbox"
+            /> Forfeit soul
           </label>
-          <input type="submit" value="Submit" />
+          <button className="btn btn-primary btn-block">
+            Sign in!
+          </button>
         </form>
       </div>
     );
