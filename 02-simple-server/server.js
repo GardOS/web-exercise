06c3b21c80697ccf4115653cfbe904e4c8e09f2b
GardOS <Gardo1506@gmail.com>
2018-10-06 16:28:59 +0000

    Use more complex object as response

--- 02-simple-server/server.js
+++ 02-simple-server/server.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const app = express();
 
-const greeting = {
-	message: "Hello!"
-}
+const fruits = [
+	{
+		index: 1,
+		fruit: 'Banana',
+		color: 'Yellow',
+	},
+	{
+		index: 2,
+		fruit: 'Orange',
+		color: 'Orange',
+	},
+	{
+		index: 3,
+		fruit: 'Apple',
+		color: 'Apple-colored',
+	}
+];
 
 app.get('/', (req, res) => {
-    res.send(greeting);
+    res.send(fruits);
 });
 
 app.listen(3000, () => console.log("listening on 3000!"));
