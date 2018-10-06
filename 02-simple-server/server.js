9be0ece2b2fb0d2fa2b68e5298ad5e60db294188
GardOS <Gardo1506@gmail.com>
2018-10-06 16:07:24 +0000

    Return object instead of string

--- 02-simple-server/server.js
+++ 02-simple-server/server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
 
+const greeting = {
+	message: "Hello!"
+}
+
 app.get('/', (req, res) => {
-    res.send("Hello");
+    res.send(greeting);
 });
 
 app.listen(3000, () => console.log("listening on 3000!"));
