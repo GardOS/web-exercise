59b9277b5732969b5a6188bcf9d763382a2bc14e
GardOS <Gardo1506@gmail.com>
2018-10-07 13:42:51 +0000

    Add endpoints to interact with DB

--- 03-simple-mongodb/server.js
+++ 03-simple-mongodb/server.js
@@ -9,12 +9,34 @@ mongoose.connect(`mongodb://gardos:${process.argv[2]}@ds245022.mlab.com:45022/03
 
 const Fruit = mongoose.model('Fruit', {
   name: { type: String, required: true },
-  color: { type: String, required: [true, 'What is a fruit without its color? - Einstein'] },
+  color: { type: String, required: [true, '\"What is a fruit without its color?\" - Einstein'] },
   taste: { type: String, enum: ['Good', 'OK', 'Bad'] }
 });
 
-app.get('/', (req, res) => {
-  res.send('hello!');
+app.get('/fruits', (req, res) => {
+  Fruit.find((err, fruits) => {
+    if (err) {
+      res.status(500).send(err);
+      return;
+    }
+
+    res.send(fruits);
+  })
+});
+
+app.post('/fruits', (req, res) => {
+  const body = req.body;
+
+  const fruit = new Fruit(body);
+
+  fruit.save((err, savedFruit) => {
+    if (err) {
+      res.status(500).send(err);
+      return;
+    }
+
+    res.send(savedFruit);
+  });
 });
 
 app.listen(8080, () => console.log("listening on 8080!"));
