af3dc96467cd649bc84bc87ab28cb90b40a979cb
GardOS <Gardo1506@gmail.com>
2018-10-07 11:21:32 +0000

    Add body-parser to server

--- 02-simple-server/server/server.js
+++ 02-simple-server/server/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const app = express();
+
 app.use('/', cors('localhost:3000'));
+app.use(bodyParser.json());
 
 const fruits = [
 	{
