9a207377f23db3e6e4a4ca002624ddec2128f87e
GardOS <Gardo1506@gmail.com>
2018-10-07 11:22:04 +0000

    Add client-server communication

--- 02-simple-server/client/src/App.js
+++ 02-simple-server/client/src/App.js
@@ -5,15 +5,40 @@ class App extends Component {
   constructor() {
     super();
 
-    this.state = { };
+    this.state = {
+      fruits: []
+    };
+  }
+
+  getFruits(){
+    fetch('http://localhost:8080/')
+      .then(res => res.json())
+      .then(fruits => this.setState({ fruits }))
+      .catch(err => console.error(err));
   }
 
   render() {
-    return (
-      <div className="container">
-        Hello!
-      </div>
-    );
+    if (this.state.fruits === null || this.state.fruits.length === 0) {
+      return (
+        <div className="container">
+          <h1>No fruits!</h1>
+          <button onClick={() => this.getFruits()}>
+            I want fruits!
+          </button>
+        </div>
+      );
+    } else {
+      return (
+        <div className="container">
+          <h1>Fruits!</h1>
+          {this.state.fruits.map(f =>
+            <div key = {f.index}>
+              {`Fruit: ${f.fruit} \t -\tcolor: ${f.color}`}
+            </div>
+          )}
+        </div>
+      );
+    }
   }
 }
 
