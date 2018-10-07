33ab8a9c9e09c5008b55697c1494b23390f08d8b
GardOS <Gardo1506@gmail.com>
2018-10-07 12:42:55 +0000

    Add mongoose connection

--- 03-simple-mongodb/server.js
+++ 03-simple-mongodb/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-
 app.use(bodyParser.json());
+
+const mongoose = require('mongoose');
+mongoose.connect(`mongodb://gardos:${process.argv[2]}@ds245022.mlab.com:45022/03-simple-mongodb`);
+
 
 
 app.get('/', (req, res) => {
