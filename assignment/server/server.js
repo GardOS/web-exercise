1960e9af9c430c947bcae547a18dbff7d1c22e9e
GardOS <Gardo1506@gmail.com>
2018-10-07 16:27:29 +0000

    Re-use server implementation from 03

--- assignment/server/server.js
+++ assignment/server/server.js
@@ -1,10 +1,39 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
-
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.send("Hello!");
+const mongoose = require('mongoose');
+mongoose.connect(`mongodb://gardos:${process.argv[2]}@ds245022.mlab.com:45022/03-simple-mongodb`,
+  { useNewUrlParser: true });
+
+const Fruit = mongoose.model('Fruit', {
+  name: { type: String, required: true },
+  color: { type: String, required: [true, '\"What is a fruit without its color?\" - Einstein'] },
+  taste: { type: String, enum: ['Good', 'OK', 'Bad'] }
+});
+
+app.get('/fruits', (req, res) => {
+  Fruit.find((err, fruits) => {
+    if (err) {
+      res.status(500).send(err);
+      return;
+    }
+    res.send(fruits);
+  })
+});
+
+app.post('/fruits', (req, res) => {
+  const body = req.body;
+  const fruit = new Fruit(body);
+
+  fruit.save((err, savedFruit) => {
+    if (err) {
+      res.status(500).send(err);
+      return;
+    }
+    res.send(savedFruit);
+  });
 });
 
 app.listen(8080, () => console.log("listening on 8080!"));
