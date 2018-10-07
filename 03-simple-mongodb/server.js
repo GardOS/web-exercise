a13fcb52803cce9ad8897a9414a766b4841d8196
GardOS <Gardo1506@gmail.com>
2018-10-07 13:32:10 +0000

    Add model

--- 03-simple-mongodb/server.js
+++ 03-simple-mongodb/server.js
@@ -4,12 +4,17 @@ const app = express();
 app.use(bodyParser.json());
 
 const mongoose = require('mongoose');
-mongoose.connect(`mongodb://gardos:${process.argv[2]}@ds245022.mlab.com:45022/03-simple-mongodb`);
+mongoose.connect(`mongodb://gardos:${process.argv[2]}@ds245022.mlab.com:45022/03-simple-mongodb`,
+  { useNewUrlParser: true });
 
-
+const Fruit = mongoose.model('Fruit', {
+  name: { type: String, required: true },
+  color: { type: String, required: [true, 'What is a fruit without its color? - Einstein'] },
+  taste: { type: String, enum: ['Good', 'OK', 'Bad'] }
+});
 
 app.get('/', (req, res) => {
-    res.send('hello!');
+  res.send('hello!');
 });
 
 app.listen(8080, () => console.log("listening on 8080!"));
