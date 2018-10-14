85e6abecc445bcae30837fe9daca1c6ea5701e1f
GardOS <Gardo1506@gmail.com>
2018-10-14 14:49:47 +0000

    Add data + retry-functionality for connecting to DB on boot

--- assignment/server/server.js
+++ assignment/server/server.js
@@ -8,12 +8,28 @@ app.use(bodyParser.json());
 
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017', { useNewUrlParser: true })
+var connectWithRetry = function() {
+  return mongoose.connect('mongodb://localhost:27017', { useNewUrlParser: true }, function(err) {
+    if (err) {
+      console.error('Failed to connect to mongo on startup - retrying in 5 sec', err);
+      setTimeout(connectWithRetry, 5000);
+    } else{
+      console.clear();
+      console.log('Connected to MongoDB container');
+    }
+  });
+};
+connectWithRetry();
 
 const Fruit = mongoose.model('Fruit', {
   name: { type: String, required: true },
   taste: { type: String, required: true, enum: ['Good', 'OK', 'Bad'] }
 });
+
+//Init data
+new Fruit({name: 'Banana', taste: 'Good'}).save();
+new Fruit({name: 'Apple', taste: 'OK'}).save();
+new Fruit({name: 'Pear', taste: 'Bad'}).save();
 
 app.get('/fruits', (req, res) => {
   Fruit.find((err, fruits) => {
