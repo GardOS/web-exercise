046da204de89fc266e95756d424fcf88a29ff98a
GardOS <Gardo1506@gmail.com>
2018-10-16 18:57:01 +0000

    Move fruits to own component

--- assignment/client/src/App.js
+++ assignment/client/src/App.js
@@ -1,50 +1,10 @@
 import React, { Component } from 'react';
-import './App.css';
-import FruitForm from './FruitForm';
-import FruitTable from './FruitTable';
+import Fruits from './Fruits';
 
 class App extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      fruits: [],
-    };
-
-    this.addFruit = this.addFruit.bind(this)
-    this.deleteFruit = this.deleteFruit.bind(this)
-  }
-
-  componentWillMount() {
-    fetch('http://localhost:8080/fruits')
-      .then(res => res.json())
-      .then(fruits => this.setState({ fruits }))
-      .catch(err => console.error(err));
-  }
-
-  addFruit(newFruit) {
-    this.setState({
-      fruits: [...this.state.fruits, newFruit]
-    })
-  }
-
-  deleteFruit(index) {
-    this.setState((prevState) => ({
-      fruits: prevState.fruits.filter((_, i) => i !== index)
-    }))
-  }
-
   render() {
     return (
-      <div className="container-fluid">
-        <h2>Fruits!</h2>
-
-        <FruitForm newFruitHandler={this.addFruit} />
-
-        <br />
-
-        <FruitTable fruits={this.state.fruits} deleteFruitHandler={this.deleteFruit}/>
-      </div>
+        <Fruits />
     );
   }
 }
