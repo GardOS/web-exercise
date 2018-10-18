96297a6a73a0638afa3a07ce5ef45266fc7c906d
GardOS <Gardo1506@gmail.com>
2018-10-18 21:09:24 +0000

    Change 404 link text

--- assignment/client/src/NotFound.js
+++ assignment/client/src/NotFound.js
@@ -5,7 +5,7 @@ export const NotFound = () => {
 	return (
 		<div className="container">
 			<h1>404 page not found</h1>
-			<h4>Oh no! Take me <Link to={"/"}><u>back</u></Link></h4>
+			<h4>Oh no! Take me <Link to={"/"}><u>home</u></Link></h4>
 		</div>
 	);
 };
