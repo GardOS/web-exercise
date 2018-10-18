0034aba2d9ffd4ba76950882249bb94a42ef90d7
GardOS <Gardo1506@gmail.com>
2018-10-18 21:06:44 +0000

    Add base components for chat

--- assignment/client/src/FruitHub.js
+++ assignment/client/src/FruitHub.js
@@ -1,13 +1,38 @@
-import React from "react";
+import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 
-export const FruitHub = () => {
-	return (
-		<div className="container">
-			<h1>This is the fruit hub</h1>
-			<h4>Take me <Link to={"/"}><u>back</u></Link></h4>
-		</div>
-	);
-};
+class FruitHub extends Component {
+	constructor() {
+		super();
+
+		this.state = {
+			messages: []
+		}
+	}
+
+	render() {
+		return (
+			<div className="container" >
+				<h1>This is the fruit hub</h1>
+
+				{this.state.messages.map((m, i) =>
+					<div>{m}</div>
+				)}
+				<h4>Take me <Link to={"/"}><u>back</u></Link></h4>
+
+				<div class="input-group">
+					<input type="text" class="form-control" placeholder="Write message here ..." />
+					<div class="input-group-append">
+						<button type="submit" class="btn btn-primary" onClick={
+							e => this.setState({
+								messages: [...this.state.messages, "Message"]
+							})
+						}>Send</button>
+					</div>
+				</div>
+			</div>
+		)
+	}
+}
 
 export default FruitHub;
